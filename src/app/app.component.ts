6d62ae5ce340a74758d214a52a5ba1ed7f30c72b
sonam <vermasonam1408@gmail.com>
2020-10-08 08:47:42 -0700

    angular qrcode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-qrcode';
+  // data = [{
+  //   'name': 'John Doe',
+  //   'profile': 'Software Developer',
+  //   'email': 'john@doe.com',
+  //   'hobby': 'coding'
+  // }]
+
+  // dataToString = JSON.stringify(this.data);
+
+  public qrCodeVal: string = null;
+  public level: "L" | "M" | "Q" | "H";
+  public width: number;
+
+  constructor() {
+    this.level = "L";
+    this.qrCodeVal = "QR code string value";
+    this.width = 200;
+  }
+
+  updateLevel(newValue: "L" | "M" | "Q" | "H") {
+    this.level = newValue;
+  }
+
+  updateQrInfo(newValue: string) {
+    this.qrCodeVal = newValue;
+  }
+
+  updateWidth(newValue: number) {
+    this.width = newValue;
+  }
+
 }
